4ea126ab5001e7f5f44e35bee0edd9414659c2d8
aastha300306 <24bit035@sot.pdpu.ac.in>
2025-01-07 16:02:54 +0530

    Add files via upload

---
+++ Add,sub,multiply,divide.py
@@ -0,0 +1,9 @@
+#Add multiply subtract and divide two numbers
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("Addition of two numbers is:",a+b)
+print("Subtraction of two numbers is:",a-b)
+print("Multiplication of two numbers is:",a*b)
+print("Division of two numbers is:",a/b)
