abb3183c6f8946b43053eee2881233af210ee05f
aastha300306 <24bit035@sot.pdpu.ac.in>
2025-01-15 20:52:44 +0530

    Add files via upload

---
+++ dollars into rupees.py
@@ -0,0 +1,4 @@
+#convert dollars into ruppees
+a=int(input("Enter the amount in dollars:"))
+a*48
+print("Amount in ruppees:", a*48)
---
+++ rupees to dollars.py
@@ -0,0 +1,4 @@
+#convert rupees into dollars
+a=int(input("Enter the amount in rupees:"))
+a/48
+print("Amount in dollars:", a/48)
