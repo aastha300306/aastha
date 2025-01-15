48c7180ac2c76a876782e1c5b11c097c408fa7a3
aastha300306 <24bit035@sot.pdpu.ac.in>
2025-01-15 20:52:03 +0530

    Add files via upload

---
+++ convert hours into mins.py
@@ -0,0 +1,4 @@
+#Convert hours into minutes
+a=int(input("Enter a number in hours:"))
+a*60
+print("Number in minutes:",a*60)
---
+++ convert mins into hours.py
@@ -0,0 +1,4 @@
+#convert minutes into hours
+a=int(input("Enter a number in minutes:"))
+a/60
+print("Number in hours:", a/60)
